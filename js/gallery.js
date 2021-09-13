90e23d0bfc6f569d2e8f7461d44c3f58bd3381d3
laurentbasset <l.basset@codeur.online>
2021-09-13 11:45:54 +0200

    first

---
+++ js/gallery.js
@@ -0,0 +1,44 @@
+let gallery = document.querySelector('#gallery');
+let projects = gallery.getElementsByClassName('project');
+gallery.addEventListener('click',function(e){
+    let t = e.target;
+   
+    if(t.classList.contains('imgProj')){
+        let par = t.parentNode;
+        toogleProject(par.parentNode)
+    }
+    if(t.classList.contains('titleProj')){
+        toogleProject(t.parentNode);
+    }
+});
+
+function toogleProject(project){
+//close all others opened projects
+   for(let j=0; j<projects.length; j++){
+        if(projects[j] != project && projects[j].classList.contains("column") ){
+            closeProject(projects[j]);
+        }
+   }
+
+// open/close project
+    if(project.classList.contains("column")){
+        setTimeout(function(){ project.classList.toggle('column'); }, 300);
+    }else{
+        project.classList.toggle('column');
+    }
+
+    let childs = project.childNodes;
+    let img = childs[1];
+    let txt = childs[5];
+    img.classList.toggle('thumb');
+    setTimeout(function(){ txt.classList.toggle('visible'); }, 300);
+}
+
+function closeProject(project){
+    setTimeout(function(){ project.classList.toggle('column'); }, 300);
+    let childs = project.childNodes;
+    let img = childs[1];
+    let txt = childs[5];
+    img.classList.toggle('thumb');
+    setTimeout(function(){ txt.classList.toggle('visible'); }, 300); 
+}
