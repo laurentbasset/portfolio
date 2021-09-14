38a7045f55d182128a7f88655a75c52ae7f859cd
laurentbasset <l.basset@codeur.online>
2021-09-14 12:01:59 +0200

    reglage scrolling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 let plus = document.getElementById('Aplus');
-let A = document.getElementById('A');
+let wraps = document.getElementsByClassName('wrap');
 plus.addEventListener('click', function(){
-    A.classList.toggle('full');
+    wraps[0].classList.toggle('full');
+    if(wraps[0].classList.contains('full')){ 
+        setTimeout( function(){ plus.innerHTML ='réduire'; }, 800);
+    }else{
+        setTimeout( function(){ plus.innerHTML ='+ d\'infos'; }, 800);
+    }
 });
+
+
+//scrolling
+// document.addEventListener('scroll', function(event){
+//     let v = document.documentElement.scrollTop;
+//     console.log(v);
+// });
+
+// let A = document.getElementById('A');
+// let B = document.getElementById('B');
+// A.addEventListener('mouseover', function(){
+//     console.log('A');
+// });
+// B.addEventListener('mouseover', function(){
+//     console.log('B');
+// });
